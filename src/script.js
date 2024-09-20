2e62a58767506d63ce053cb3595ea1ca106cf2e8
Bharadwaja sahoo <bharadwajasahoo96@gmail.com>
2024-09-20 23:46:40 +0530

    pocket

--- src/script.js
+++ src/script.js
@@ -40,4 +40,16 @@ $(document).ready(function () {
         $('#hero').html(heroContent);
         scroll.update();
     });
+    document.querySelector('#container').addEventListener('mousemove', eyeball);
+    function eyeball() {
+        var eye = document.querySelectorAll('.eye');
+        eye.forEach(function (eye) {
+            let x = (eye.getBoundingClientRect().left) + (eye.clientWidth / 2);
+            let y = (eye.getBoundingClientRect().top) + (eye.clientHeight / 2);
+
+            let radian = Math.atan2(event.pageX - x, event.pageY - y);
+            let rotation = (radian * (180 / Math.PI) * -1) + 360;
+            eye.style.transform = "rotate(" + rotation + "deg)"
+        });
+    }
 });
